91c7dc2ced14a7fdc1a68b4b779b1d26070f03a6
jiro4989 <jiroron666@gmail.com>
2020-09-05 14:54:38 +0000

    sample code

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"image"
+	"image/color"
+
+	"github.com/llgcode/draw2d/draw2dimg"
 )
 
 const usage = `layoutimg is TODO
@@ -25,5 +28,30 @@ Options:
 `
 
 func main() {
-	fmt.Println("hello")
+	dest := image.NewRGBA(image.Rect(0, 0, 400, 200))
+	drawBackground(dest, color.RGBA{255, 255, 255, 255})
+
+	dp := drawParam{
+		x:           0,
+		y:           0,
+		column:      4,
+		row:         2,
+		pad:         5,
+		strokeColor: color.RGBA{0, 0, 0, 255},
+		lineWidth:   3,
+	}
+	draw(dest, dp)
+
+	dp = drawParam{
+		x:           1,
+		y:           0,
+		column:      4,
+		row:         2,
+		pad:         5,
+		strokeColor: color.RGBA{0, 0, 0, 255},
+		lineWidth:   3,
+	}
+	draw(dest, dp)
+
+	draw2dimg.SaveToPngFile("out.png", dest)
 }
