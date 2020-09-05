18b0822ab929747e5b85734f6c0f9b3d582bb6c8
jiro4989 <jiroron666@gmail.com>
2020-09-05 13:56:55 +0000

    implement draw

--- image.go
+++ image.go
@@ -7,25 +7,37 @@ import (
 	"github.com/llgcode/draw2d/draw2dimg"
 )
 
-type param struct {
+type drawParam struct {
 	x, y, column, row, pad int
 
 	strokeColor color.RGBA
 	lineWidth   float64
 }
 
-func draw(dest *image.RGBA, p param) {
+func draw(dest *image.RGBA, p drawParam) {
 	gc := draw2dimg.NewGraphicContext(dest)
 
-	// rp := rectParam{
-	// 	x: p.x,
-	// 	y: p.y,
-	// 	column: p.column,
-	// 	row: p.row,
-	// }
-	// r := rectangle(rp)
+	rp := rectParam{
+		x:      p.x,
+		y:      p.y,
+		column: p.column,
+		row:    p.row,
+	}
+	r := rectangle(rp)
 
 	gc.SetStrokeColor(p.strokeColor)
 	gc.SetLineWidth(p.lineWidth)
+
+	// left top
+	gc.MoveTo(float64(r.Min.X), float64(r.Min.Y))
+	gc.LineTo(float64(r.Max.X-r.Min.X), float64(r.Min.Y))
+	gc.LineTo(float64(r.Min.X), float64(r.Max.Y-r.Min.Y))
+
+	// right bottom
+	gc.MoveTo(float64(r.Max.X), float64(r.Max.Y))
+	gc.LineTo(float64(r.Min.X-r.Max.X), float64(r.Max.Y))
+	gc.LineTo(float64(r.Max.X), float64(r.Min.Y-r.Max.Y))
+
 	gc.Close()
+	gc.FillStroke()
 }
