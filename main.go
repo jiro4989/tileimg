56675233521eaa59aac724d8ff1988e1cf2ef174
jiro4989 <jiroron666@gmail.com>
2020-09-05 12:36:19 +0000

    add usage

--- main.go
+++ main.go
@@ -4,6 +4,26 @@ import (
 	"fmt"
 )
 
+const usage = `layoutimg is TODO
+
+Usage:
+  layoutimg [options]
+  layoutimg -h | --help
+  layoutimg --version
+
+Options:
+  -h, --help
+      --version
+  -W, --width <width>
+  -H, --height <height>
+  -c, --column <column>
+  -r, --row <row>
+  -b, --background-color <background-color>
+  -l, --line-color <line-color>
+  -f, --fill-color <fill-color>
+
+`
+
 func main() {
 	fmt.Println("hello")
 }
